6ac9e39c69462b8f5de9004d9e22dcc54a142ba3
Ayoub Senhaji <ayoubsenhajidev@gmail.com>
2021-04-05 13:10:21 +0100

    about

--- pages/about.js
+++ pages/about.js
@@ -2,18 +2,19 @@ import { Box, Flex } from "@chakra-ui/react";
 
 function about() {
   return (
-    <Box padding='300px'>
-      <Flex
-        border='2px solid black'
-        justifyContent='center'
-        flexDirection='column'
-        alignItems='center'
-      >
+    <Box padding='100px' maxW='900px' margin='0 auto'>
+      <Flex justifyContent='center' flexDirection='column' alignItems='center'>
         <Box fontSize='40px' fontWeight='bold'>
           <p>Who are we ?</p>
         </Box>
         <Box fontWeight='bold'>
-          <p></p>
+          <p>
+            an IT company dedicated to providing the best services to it's
+            customers ranging from image doctoring to cyber security. We use
+            cutting edge technology carefully crafted by professionals with the
+            highest standards such as AI robotics to meet your demands in the
+            most satusfying manner possible.
+          </p>
         </Box>
       </Flex>
     </Box>
